bfa4bacb90e007b412f732aca41cf3cedee6ed94
yetaeng <yetaeng20@gmail.com>
2022-08-01 15:51:54 +0900

    addForm 컴포넌트를 함수형으로 변경

--- habit-tracker/src/components/habitAddForm.jsx
+++ habit-tracker/src/components/habitAddForm.jsx
@@ -1,28 +1,26 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
-class HabitAddForm extends PureComponent {
-    inputRef = React.createRef();
+const HabitAddForm = memo((props) => {
+    const inputRef = React.createRef();
 
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        let name = this.inputRef.current.value;
-        name && this.props.onAdd(name);
-        this.inputRef.current.value = ''; // formRef를 이용해도 된다(정석)
+        let name = inputRef.current.value;
+        name && props.onAdd(name);
+        inputRef.current.value = ''; // formRef를 이용해도 된다(정석)
     };
 
-    render() {
-        return (
-            <form className="add-form" onSubmit={this.onSubmit}>
-                <input
-                    type="text"
-                    className="add-input"
-                    placeholder="Habit"
-                    ref={this.inputRef}
-                />
-                <button className="add-button">Add</button>
-            </form>
-        );
-    }
-}
+    return (
+        <form className="add-form" onSubmit={onSubmit}>
+            <input
+                type="text"
+                className="add-input"
+                placeholder="Habit"
+                ref={inputRef}
+            />
+            <button className="add-button">Add</button>
+        </form>
+    );
+});
 
 export default HabitAddForm;
