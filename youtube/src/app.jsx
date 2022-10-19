46057f124bd66e9b28e283b57017dc104ec717ea
yetaeng <yetaeng20@gmail.com>
2022-10-20 01:54:05 +0900

    add sessionStorage

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -6,14 +6,25 @@ import Main from './pages/main';
 import Search from './pages/search';
 import Watch from './pages/watch';
 
+let defaultVideos = JSON.parse(sessionStorage.getItem('defaultVideos')) || [];
 function App({ youtubeService }) {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, setVideos] = useState(() => 
+    JSON.parse(sessionStorage.getItem('videos')) || []
+  );
+  const [selectedVideo, setSelectedVideo] = useState(() => 
+    JSON.parse(sessionStorage.getItem('selectedVideo')) || null
+  );
   const navigate = useNavigate();
 
-  // 수정 필요
+  useEffect(() => {
+    sessionStorage.setItem('videos', JSON.stringify(videos));
+    sessionStorage.setItem('selectedVideo', JSON.stringify(selectedVideo));
+    sessionStorage.setItem('defaultVideos', JSON.stringify(defaultVideos));
+  }, [videos, selectedVideo])
+
   const onClickLogo = () => {
     navigate('/');
+    setVideos(defaultVideos);
   }
 
   const handleSearch = useCallback((query) => {
@@ -30,12 +41,15 @@ function App({ youtubeService }) {
   }
 
   useEffect(() => {
-    youtubeService
-      .mostPopular()
-      .then((videos) => {
-        setVideos(videos);
-      })
-  }, [youtubeService]);
+    if (defaultVideos.length === 0) {
+        youtubeService
+          .mostPopular()
+          .then((videos) => {
+            setVideos(videos);
+            defaultVideos = videos;
+          });
+      }
+    }, [youtubeService]);
 
   return (
     <div className={styles.app}>
