ff8176846a3bb3a93a73976bbf78b27f464d193b
yetaeng <yetaeng20@gmail.com>
2022-10-20 18:37:40 +0900

    add 명함 이미지 다운로드 기능

--- card-maker/src/components/cardPreviewItem/cardPreviewItem.jsx
+++ card-maker/src/components/cardPreviewItem/cardPreviewItem.jsx
@@ -1,18 +1,37 @@
-import React, { memo } from 'react';
+import React, { useState, memo, useRef } from 'react';
+import domToImage from 'dom-to-image';
+import { saveAs } from 'file-saver';
 import styles from './cardPreviewItem.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 const CardPreviewItem = memo(({ card }) => {
     const { name, company, color, title, email, message, fileURL } = card;
+    const [isHover, setIsHover] = useState(false);
+    const cardRef = useRef();
+    const downloadRef = useRef();
+
+    const onDownload = () => {
+        const dom = cardRef.current;
+        const filter = (dom) => {
+            return dom.tagName !== "svg";
+        }
+
+        domToImage.toBlob(dom, { filter }).then((blob) => saveAs(blob, `${name}_${company}.png`));
+    };
     
     return (
-        <li className={`${styles.cardPreviewItem} ${getStyles(color)}`}>
-            <img className={styles.profile} src={fileURL || './images/default_logo.png' } alt="profile_img" />
-            <div className={styles.infos_area}>
-                <p className={styles.name}>{name}</p>
-                <p className={styles.company}>{company}</p>
-                <p>{title}</p>
-                <p>{email}</p>
-                <p>{message}</p>
+        <li className={styles.wrap} >
+            <div ref={cardRef} className={`${styles.cardPreviewItem} ${getStyles(color)}`} onMouseEnter={()=>{setIsHover(true)}} onMouseLeave={()=>{setIsHover(false)}}>
+                <img className={styles.profile} src={fileURL || './images/default_logo.png' } alt="profile_img" />
+                <div className={styles.infos_area}>
+                    <p className={styles.name}>{name}</p>
+                    <p className={styles.company}>{company}</p>
+                    <p>{title}</p>
+                    <p>{email}</p>
+                    <p>{message}</p>
+                </div>
+                {isHover && <FontAwesomeIcon className={styles.download} ref={downloadRef} icon={faDownload} onClick={onDownload}/>}
             </div>
         </li>
     );
